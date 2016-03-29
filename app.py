3d8fa0f48b403f8f72e6c3d174ff637a3c64a432
Pierre Bhoorasingh <bhoorasingh.p@husky.neu.edu>
2016-03-29 01:40:13 -0400

    Add some `bootstrap` implementation

--- app.py
+++ app.py
@@ -9,13 +9,12 @@ from bokeh.charts import Bar
 from bokeh.embed import components
 from bokeh.models import FactorRange
 from bokeh.resources import INLINE
-from bokeh.models.widgets import Select
+from bokeh.models.widgets import Select, Slider
 from bokeh.palettes import RdBu11, YlGn9
 
 app = Flask(__name__)
 
 js_resources = INLINE.render_js()
-css_resources = INLINE.render_css()
 
 TOOLS="pan,wheel_zoom,box_zoom,reset,save"
 
@@ -37,10 +36,10 @@ def index():
                           toolbar_location="below"
                           )
         
-        plot.line(df_tranfer['yr'], df_tranfer['fee_pounds'],color=RdBu11[0],line_width=4)
+        plot.line(df_tranfer['year'], df_tranfer['fee_pounds'],color=RdBu11[0],line_width=4)
         
         script, div = components(plot)
-        return render_template('index.html', js_resources=js_resources, css_resources=css_resources, script=script, div=div)
+        return render_template('index.html', js_resources=js_resources, script=script, div=div)
     else:
         if request.form['submit'] == "Professional League Data":
             return redirect('/league_data') 
@@ -72,14 +71,18 @@ def leagues():
         
         
         df_squads = pd.read_csv('wc_squads.csv')
-        grouped = df_squads.groupby('WC_Year')
-        club_country = df_squads.Club_Country.unique()
+        df_squads = df_squads.replace(to_replace="Korea Republic", value="South Korea")
+        df_squads = df_squads.replace(to_replace="West Germany", value="Germany")
+        df_squads = df_squads.replace(to_replace="Dutch East Indies (Indonesia)", value="Dutch East Indies")
+
+        grouped = df_squads.groupby('year')
+        club_country = df_squads.club_country.unique()
         club_country.sort()
         df_clb_ctry = pd.DataFrame({"club_country":club_country})
         dfdf_clb_ctry2 = df_clb_ctry.set_index("club_country")
         
-        for year in df.WC_Year.unique():
-            country_to_club = grouped.Club_Country.value_counts()[year] / grouped.Country.value_counts()[year]
+        for year in df_squads.year.unique():
+            country_to_club = grouped.club_country.value_counts()[year] / grouped.team.value_counts()[year]
             df_clb_ctry = df_clb_ctry.join(country_to_club.to_frame(year))
             
         df_clb_ctry = df_clb_ctry.fillna(0) # If NaN, country league not represented at the world cup
@@ -92,7 +95,7 @@ def leagues():
                       )
         
         # take the top 11 countries and plot
-        for num, country in enumerate(ddf_clb_ctryf2.sum().sort_values(ascending=False).head(11).index):
+        for num, country in enumerate(df_clb_ctry.sum().sort_values(ascending=False).head(11).index):
             plot2.scatter(df_clb_ctry.index, df_clb_ctry[country],legend=country,color=RdBu11[num],size=8, alpha=0.5)
         
         plot2.legend.location = "top_left"
@@ -112,7 +115,7 @@ def leagues():
         
         script, div = components(plot_dict)
         
-        return render_template('league.html', js_resources=js_resources, css_resources=css_resources, script=script, div=div)
+        return render_template('league.html', js_resources=js_resources, script=script, div=div)
     else:
         if request.form['submit'] == "Player Data":
             return redirect('/player_data') 
@@ -125,14 +128,18 @@ def players():
         month_map = {k: v for k,v in enumerate(calendar.month_abbr)}
         
         df_squads = pd.read_csv('wc_squads.csv')
-        df_squads.DoB = pd.to_datetime(df.DoB)
-        df_squads['birth_month'] = df_squads.DoB.map(lambda x: x.month)
+        df_squads = df_squads.replace(to_replace="Korea Republic", value="South Korea")
+        df_squads = df_squads.replace(to_replace="West Germany", value="Germany")
+        df_squads = df_squads.replace(to_replace="Dutch East Indies (Indonesia)", value="Dutch East Indies")
+
+        df_squads.dob = pd.to_datetime(df_squads.dob)
+        df_squads['birth_month'] = df_squads.dob.map(lambda x: x.month)
         df_mth = df_squads.birth_month.value_counts().sort_index()
         df_mth.index = df_mth.index.map(lambda x: month_map[x])
         
-        grouped = df_squads.groupby('WC_Year')
+        grouped = df_squads.groupby('year')
         
-        avg_age = df_squads.groupby('WC_Year').Age.mean().to_frame()
+        avg_age = df_squads.groupby('year').age.mean().to_frame()
         
         plot1 = figure(tools=TOOLS,
                       title='Average Team Age',
@@ -140,9 +147,9 @@ def players():
                       toolbar_location="below"
                       )
     
-        plot1.line(avg_age.index, avg_age.Age.values, color='green', line_width=4, alpha=0.5)
+        plot1.line(avg_age.index, avg_age.age.values, color='green', line_width=4, alpha=0.5)
         
-        # select = Select(title="World Cup Year:", value="all", options=["all"] + map(str, df.WC_Year.unique().tolist()))
+        # select = Select(title="World Cup Year:", value="all", options=["all"] + map(str, df.year.unique().tolist()))
         
         plot2 = Bar(df_mth, toolbar_location="below")
         plot2.x_range = FactorRange(factors=df_mth.index.tolist())
@@ -152,7 +159,7 @@ def players():
         script, div = components(plots)
         
         
-        return render_template('player.html', js_resources=js_resources, css_resources=css_resources, script=script, div=div)
+        return render_template('player.html', js_resources=js_resources, script=script, div=div)
         
     else:
         if request.form['submit'] == "Professional League Data":
